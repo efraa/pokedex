7f7716c1f3469037662caac3ca2aecee9fc31f2f
Efra Peralta <efra23peralta@gmail.com>
2019-11-05 09:03:30 -0400

    feat: remove defaults headers

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,23 @@ import { apiRoute, headers } from './apiConnection'
 const api = instance.create({
   baseURL: apiRoute,
   headers
-});
+})
 
 export const post = async (endpoint, data) =>
-  await api.post(endpoint, data)
+  await api.post(endpoint, data, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: localStorage.token,
+    }
+  })
 
 export const put = async (endpoint, data) =>
-  await api.put(endpoint, data)
+  await api.put(endpoint, data, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: localStorage.token,
+    }
+  })
 
 export const get = async endpoint =>
   await api.get(endpoint)
